f39a181f690f3230463a8bcc4a5c54422dbb3821
Andressa Gomes <ryuu.tera81@gmail.com>
2018-10-23 11:15:28 -0300

    desafio01
    aula23-10-2018

---
+++ TRABALHO01.js
@@ -0,0 +1,10 @@
+
+function primeirapagina (){
+	document.getElementById('conteudo2').style.display = "none";
+	document.getElementById('conteudo1').style.display = "block";
+}
+
+function segundapagina (){
+	document.getElementById('conteudo2').style.display = "block";
+	document.getElementById('conteudo1').style.display = "none";
+}
